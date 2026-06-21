fef0c152d443d85e50d31bc1290dbc4a55f9b044
John, Sing Dao, Siu <john.sd.siu@gmail.com>
2026-06-21 14:44:51 -0400

    feat: support non-semver

--- lib/helper.go
+++ lib/helper.go
@@ -25,23 +25,96 @@ THE SOFTWARE.
 package lib
 
 import (
+	"slices"
+	"strconv"
 	"strings"
 
 	"github.com/J-Siu/go-helper/v2/ezlog"
-	"golang.org/x/mod/semver"
 )
 
-func VerNewer(v1, v2 string) bool {
-	prefix := "VerCompare"
-	if !strings.HasPrefix(v1, "v") {
-		v1 = "v" + v1
+const VerDelimiters = "._-"
+
+// return v1 > v2
+func VerNewer(v1, v2 string) (newer bool) { return segmentCompare(segmentSplit(v1), segmentSplit(v2)) }
+
+// return s1 > s2
+func segmentCompare(s1, s2 []string) (newer bool) {
+	prefix := "SegmentCompare"
+	var (
+		e     error
+		equal = true
+		int1  int
+		int2  int
+		len1  = len(s1)
+		len2  = len(s2)
+	)
+	for i := range min(len1, len2) {
+		ezlog.Debug().N(prefix).N("s1").N(i).M(s1[i]).Out()
+		ezlog.Debug().N(prefix).N("s2").N(i).M(s2[i]).Out()
+		int1, e = strconv.Atoi(trimNonNumeric(s1[i]))
+		if e == nil {
+			int2, e = strconv.Atoi(trimNonNumeric(s2[i]))
+		}
+		if e == nil {
+			equal = equal && int1 == int2
+			newer = int1 > int2
+		}
+		if e != nil || !equal {
+			break
+		}
 	}
-	if !strings.HasPrefix(v2, "v") {
-		v2 = "v" + v2
+
+	if e == nil && equal {
+		newer = len1 > len2
 	}
-	newer := semver.Compare(v1, v2) > 0
-	ezlog.Debug().N(prefix).N("new").M(v1).Out()
-	ezlog.Debug().N(prefix).N("old").M(v2).Out()
-	ezlog.Debug().N(prefix).N("semver.Compare > 0").M(newer).Out()
 	return newer
+}
+
+// Trim leading 'v', split s by VerDelimiters
+func segmentSplit(s string) (segments []string) {
+	var (
+		char = ""
+	)
+	s = strings.TrimPrefix(s, "v")
+	s = strings.TrimPrefix(s, "V")
+	segments = []string{}
+	for l := range len(s) {
+		char = string(s[l])
+		if strings.Contains(VerDelimiters, char) {
+			segments = append(segments, "")
+		} else {
+			if len(segments) == 0 {
+				segments = append(segments, "")
+			}
+			segments[len(segments)-1] += char
+		}
+	}
+	return segments
+}
+
+func trimNonNumeric(s string) string {
+	prefix := "trimNonNumeric"
+	var (
+		start int
+		end   int
+	)
+	// get start
+	for i, c := range s {
+		start = i
+		if c >= '0' && c <= '9' {
+			break
+		}
+	}
+	// get end
+	for i, c := range slices.Backward([]byte(s)) {
+		end = i
+		if c >= '0' && c <= '9' {
+			break
+		}
+	}
+	end++
+
+	ezlog.Debug().N(prefix).N("s").M(s).N("start").M(start).N("end").M(end).Out()
+	ezlog.Debug().N(prefix).N("out").M(s[start:end]).Out()
+	return s[start:end]
 }
